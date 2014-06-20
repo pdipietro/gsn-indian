d5b295f32c87940cf650d5ddea061ebceafe57fa
Neo4j-Ubuntu64 <paolodipietro58@gmail.com>
2014-06-20 17:33:41 +0200

    added chown logfiles  after loading the DB

--- db_load.sh
+++ db_load.sh
@@ -9,3 +9,5 @@ sudo cat $HOME/gsn-data/load_domain_data/countries.cypher.txt | ./neo4j/bin/neo4
 sudo cat $HOME/gsn-data/load_domain_data/sex.cypher.txt | ./neo4j/bin/neo4j-shell neo4j.properties -path ./neo4j/data/graph.db >> ./log/db_load.log	  2>>./log/db_load_stderr.log
 sudo cat $HOME/gsn-data/load_domain_data/languages.cypher.txt | ./neo4j/bin/neo4j-shell neo4j.properties -path ./neo4j/data/graph.db >> ./log/db_load.log	  2>>./log/db_load_stderr.log
 
+sudo chown -R pdipietro:pdipietro ./neo4j/data/graph.db ./log/db_load.log ./log/db_load_stderr.log
+
